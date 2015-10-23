720728e5641996c9caff8dd9d514efb134350d4e
Adam D <addstar34@gmail.com>
2015-10-23 01:02:44 +0000

    setup the course detail page

--- test/controllers/instructor/courses_controller_test.rb
+++ test/controllers/instructor/courses_controller_test.rb
@@ -1,15 +1,17 @@
 require 'test_helper'
 
 class Instructor::CoursesControllerTest < ActionController::TestCase
-	# test "create course" do
-	# 	user = FactoryGirl.create(:user)
-	# 	sign_in user
-	# 	assert_difference('Course.count') do
-	# 		post :create, :user_id => user.id, :course => {
-	# 			:title => "Course Title",
-	# 			:description => "Course description goes here",
-	# 			:cost => "1.99"
-	# 		}
-	# 	end
-	# end
+	test "create course" do
+		user = FactoryGirl.create(:user)
+		sign_in user
+		assert_difference('Course.count') do
+			post :create, :user_id => user.id, :course => {
+				:title => "Course Title",
+				:description => "This is the course description.",
+				:cost => "1.99"
+			}
+		end
+		assert_equal 1, Course.count
+		assert_redirected_to instructor_course_path(Course.first)
+	end
 end
