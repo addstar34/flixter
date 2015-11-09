119e6d442b95c5489488623ada74047c28010f2a
Adam D <addstar34@gmail.com>
2015-11-09 06:26:33 +0000

    updated footer and homepage

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,10 +7,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  if Rails.env.production?
+    storage :aws
+  else
+    storage :file
+  end
   # storage :fog
   # :aws doesn't work on windows inside a vagrant environment
-  storage :aws
+  # storage :aws
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
